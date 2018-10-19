a131dd590023918120c619f2466627eb4bbc1a48
JungMongKyo <dreamong91@gmail.com>
2018-10-19 16:22:22 +0900

    posts.views.post_create, posts/post_create.html 연결 및 템플릿 form구현

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 
 from . import views
 
+# 중복되는 것을 방지시켜줌
 app_name = 'posts'
 
+
+# config.urls에 연결되는 것이기 때문에 / 기호는 뒷쪽에 붙여주어야한다.
 urlpatterns = [
     path('', views.post_list, name='post-list'),
+    path('create/', views.post_create, name='post-create')
 ]
+
+# 2. /posts/create/ URL에 이 view를 연결
+#    URL명은 'post-create'를 사용
