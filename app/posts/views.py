9cd7546679c082a41ff98b7b4a08412f1a106d1f
JungMongKyo <dreamong91@gmail.com>
2018-10-19 14:21:52 +0900

    posts.views.post_list와 post.urls, config.urls간의 연결 구성

--- app/posts/views.py
+++ app/posts/views.py
@@ -10,15 +10,15 @@ def post_list(request):
     #     두 필드를 추가
 
     # 2. Post모델이 기본적으로 pk 내림차순으로 정렬되도록 설정
-    posts = Post.objects.order_by('-pk')
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'post_list.html', context)
     # 3. 모든 Post객체에 대한 QuerySet을
     #    render의 context인수로 전달 (키 : posts)
-
 
     # 4. posts/post_list.html을 Template으로 사용
     #       템플릿에서는 posts값을 순회하며
     #       각 Post의 photo정보를 출력
+
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/post_list.html', context)
