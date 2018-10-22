ef4c87808ed38fe50f93624c08a5849c0be678d1
JungMongKyo <dreamong91@gmail.com>
2018-10-22 13:03:07 +0900

    login view, url, template구현

--- app/members/urls.py
+++ app/members/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from . import views
 
 # 중복되는 것을 방지시켜줌
-app_name = 'posts'
+app_name = 'members'
 
 
 # config.urls에 연결되는 것이기 때문에 / 기호는 뒷쪽에 붙여주어야한다.
 urlpatterns = [
-    path('', views.post_list, name='post-list'),
-    path('create/', views.post_create, name='post-create')
+    path('login/', views.login_view, name='login'),
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def login_view(request):
+    # URL: /members/login/
+    #   config.urls에서 '/members/'부분을 'members.urls'를 사용하도록 include
+    #   members.urls에서 '/login/'부분을 이 view에 연결
+
+    # Template: members/login.html
+    #  템플릿의 GET요청시 아래의 LoginForm인스턴스를 사용
+    #  POST요청시의 처리는 아직 하지 않음
+
+    # Form: members/forms.py
+    #   LoginForm
+    #     Username, password를 받을 수 있도록 함
+    #       password는 widget에 PasswordInput을 사용하기
+    if request.method == 'POST':
+        pass
+    else:
+        form = LoginForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'members/login.html', context)
