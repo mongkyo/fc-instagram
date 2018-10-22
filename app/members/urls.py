dda540e2d684b98cdad00b8bfe2b7c36426a8329
JungMongKyo <dreamong91@gmail.com>
2018-10-22 15:43:50 +0900

    signup view, url, template연결

--- app/members/urls.py
+++ app/members/urls.py
@@ -9,5 +9,6 @@ app_name = 'members'
 # config.urls에 연결되는 것이기 때문에 / 기호는 뒷쪽에 붙여주어야한다.
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .forms import LoginForm
+from .forms import LoginForm, SignupForm
 from django.contrib.auth import authenticate, login, logout
 
 
@@ -55,3 +55,23 @@ def logout_view(request):
     if request.method == 'POST':
         logout(request)
         return redirect('posts:post-list')
+
+
+def signup_view(request):
+    # URL: /members/signup/
+    # Template: members/singup.html
+    # Form:
+    #  SignupForm
+    #    username, password1, password2를 받음
+    # 나머지 요소들은 login.html의 요소를 최대한 재활용
+
+    # GET요청시 해당 템플릿 보여주도록 처리
+    # base.html에 있는 'Signup'버튼이 이 쪽으로 이동할 수 있도록 url 링크 걸기
+    if request.method == 'POST':
+        pass
+    else:
+        form = SignupForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'members/signup.html', context)
