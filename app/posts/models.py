6ecc656d769a092263dd2723ddd786e93bd0d8c2
JungMongKyo <dreamong91@gmail.com>
2018-10-26 11:25:53 +0900

    Comment._html필드 추가 및 save()에서 해당 필드 자동으로 채우도록 함

--- app/posts/models.py
+++ app/posts/models.py
@@ -47,20 +47,51 @@ class Comment(models.Model):
         verbose_name='해시태그 목록'
     )
 
+    # Comment의 save()가 호출 될 때,
+    # content의 값을 사용해서 이 필드를 자동으로 채운 후 저장하기
+    _html = models.TextField('태그가 HTML화된 댓글 내용', blank=True)
+
     class Meta:
         verbose_name = '댓글'
         verbose_name_plural = f'{verbose_name} 목록'
 
     def save(self, *args, **kwargs):
-        # DB에 변경내역을 기록한 상태
-        super().save(*args, **kwargs)
+        def save_html():
+            self._html = re.sub(
+                self.TAG_PATTERN,
+                r'<a href="/explore/tags/\g<tag>/">#\g<tag></a>',
+                self.content,
+            )
 
-        # DB에 Comment저장이 완료된 후,
-        # 자신의 'content'값에서 해시태그 목록을 가져와서
-        # 자신의 'tags'속성 (MTM필드)에 할당
-        tags = [HashTag.objects.get_or_create(name=name)[0]
-                for name in re.findall(self.TAG_PATTERN, self.content)]
-        self.tags.set(tags)
+        def save_tags():
+            tags = [HashTag.objects.get_or_create(name=name)[0]
+                    for name in re.findall(self.TAG_PATTERN, self.content)]
+            self.tags.set(tags)
+
+        save_html()
+        super().save(*args, **kwargs)
+        save_tags()
+    @property
+    def html(self):
+        # 자신의 content속성값에서
+        # "#태그명"에 해당하는 문자열을
+        #  아래와 같이 변경
+        #   <a href="/explore/tags/{태그명}>/">#{태그명}</a>
+        # re.sub를 사용
+        return self._html
+        # 템플릿에서는 comment.content대신 comment.html을 출력
+
+        # 숙제
+        #  /explore/tags/{태그명}/ URL에서
+        #  해당 태그를 가진 Posttag목록을 보여주는 view, url, template구현
+        #  URL name: tag-post-list
+        #  view:
+        #       tag_post_list(request, tag_name)
+        #  template:
+        #       /posts/tag_post_list.html
+
+        # base.html에 있는 검색창에 값을 입력하고 Enter시 (Submit)
+        #  해당 값을 사용해 위에서 만든 view로 이동
 
 
 class HashTag(models.Model):
